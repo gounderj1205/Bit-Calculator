65c8940fee0ecd9675091af4faecc8d91ea5263e
gounderj1205 <101751162+gounderj1205@users.noreply.github.com>
2022-05-20 14:34:03 +1200

    Add files via upload

--- 00_bit_calc.py
+++ 00_bit_calc.py
@@ -15,5 +15,61 @@ def statement_generator(text, decoration) :
 
     return ""
 
+
+# checks user choice is 'integer', 'text' or 'image'
+def user_choice():
+
+    # lists of valid responses
+    text_ok = ["text", "t", "txt"]
+    integer_ok = ["integer", "int", "#", "number"]
+    image_ok = ["image", "img", "pix", "picture", "pic"]
+
+    valid = False 
+    while not valid:
+
+        # ask user for choice and change response to lower case
+        response = input("file type (integer / text / image): ").lower() 
+
+        # checks for valid response and returns text, integer or image
+        
+        if response in text_ok:
+           return "text"
+            
+        elif response in integer_ok:
+          return "integer"
+
+        elif response in image_ok:
+            return "image"
+
+        elif response == "i":
+            want_integer = input("press <enter> for an integer or any key for")
+            if want_integer == "":
+                return "integer"
+            else:
+                return "image"
+        
+        else:
+            # if response is not valid. output and error
+            print ("please choose a valid file type!")
+            print()
+
+
 # Main routine goes here
-statement_generator ("look - stars", "*")
+
+# heading
+statement_generator (" Bit Calculator for integers, Text & Images", "-")
+
+# Display instrucctions if user has not used the program before
+
+# loop to allow multiple calculations per session
+keep_going = ""
+while keep_going == "":
+
+    # ask the user for the file type
+    data_type = user_choice()
+    print("you chose", data_type)
+
+    # For integers, ask for integer
+    # (must be an integer more than / equal to 0)
+
+    # For images, ask for width and height
